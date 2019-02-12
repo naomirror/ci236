ea939313645787cb96a1d153e670b4a7710d83eb
naomirror <46870555+naomirror@users.noreply.github.com>
2019-02-12 14:55:53 +0000

    prototype upload

---
+++ music/Assets/scripts/soundOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundOnClick : MonoBehaviour {
+    AudioSource self;
+	// Use this for initialization
+	void Start () {
+        self = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        self.Play();
+        self.SetScheduledEndTime(AudioSettings.dspTime + 1.50f);
+    }
+}
