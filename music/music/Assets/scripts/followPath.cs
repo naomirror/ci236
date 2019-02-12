2fd1103e3c032db71243321cf3a927a1db135d02
naomirror <46870555+naomirror@users.noreply.github.com>
2019-02-12 15:58:47 +0000

    2nd prototyping

---
+++ music/music/Assets/scripts/followPath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPath : MonoBehaviour {
+    float walkspeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePosition.x, mousePosition.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit)
+            {
+                print(hit.collider.gameObject.name);
+
+            }
+        }
+    }
+}
